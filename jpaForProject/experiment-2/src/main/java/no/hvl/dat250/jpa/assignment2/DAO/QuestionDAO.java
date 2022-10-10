8823c96bd4c024155ab23fc4dc93199299315707
polsevev <rolf.martin@glomsrud.no>
2022-10-10 10:47:01 +0200

    fixed question on poll removal

--- jpaForProject/experiment-2/src/main/java/no/hvl/dat250/jpa/assignment2/DAO/QuestionDAO.java
+++ jpaForProject/experiment-2/src/main/java/no/hvl/dat250/jpa/assignment2/DAO/QuestionDAO.java
@@ -1,6 +1,7 @@
 package no.hvl.dat250.jpa.assignment2.DAO;
 
 import no.hvl.dat250.jpa.assignment2.Answer;
+import no.hvl.dat250.jpa.assignment2.Poll;
 import no.hvl.dat250.jpa.assignment2.Question;
 
 import javax.persistence.EntityManager;
@@ -14,6 +15,9 @@ public class QuestionDAO extends DAO<Question>{
 
     public void delete(Question entity) {
         EntityTransaction tx = entityManager.getTransaction();
+        Poll poll = entity.getPoll();
+        poll.getQuestions().remove(entity);
+        new PollDAO(entityManager).update(poll);
         tx.begin();
         for (Answer a : entity.getAnswers()) {
             entityManager.remove(a);
